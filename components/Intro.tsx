fda22e8099c003c5c4ee0b8c1068e13850d507d4
Aung Kaung Myat <115795473+aungkaungmyat9504@users.noreply.github.com>
2024-05-13 11:32:12 +0700

    -

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -106,7 +106,7 @@ const Intro = () => {
 
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com/aungkaungmyat-is-coding"
+          href="https://github.com/akm-coding"
           target="_blank"
         >
           <FaGithubSquare />
