929931d38a4ef25aa7213baf410619456d9986f0
Aung Kaung Myat <115795473+Aung-kaung-myat-coding@users.noreply.github.com>
2025-01-22 22:14:34 +0700

    feat: rename resume name

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -88,7 +88,7 @@ const Intro = () => {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/AKM_Resume.pdf"
+          href="/Aung_Kaung_Myat_Web_And_Mobile_Developer.pdf"
           download
         >
           Download CV
